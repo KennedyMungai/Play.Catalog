05c772e40d0c186030d3a16d59325776f2557ec6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-02 20:17:26 +0300

    Added two extra lists to the items list

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -10,6 +10,27 @@ public class ItemsController : ControllerBase
 {
     private static readonly List<ItemDto> items = new()
     {
-        new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow)
+        new ItemDto(
+            Guid.NewGuid(), 
+            "Potion", 
+            "Restores a small amount of HP", 
+            5, 
+            DateTimeOffset.UtcNow),
+
+        new ItemDto(
+            Guid.NewGuid(),
+            "Antidote",
+            "Cures poison",
+            7,
+            DateTimeOffset.UtcNow
+        ),
+
+        new ItemDto(
+            Guid.NewGuid(),
+            "Bronze Sword",
+            "Deals a small amount of damage",
+            5,
+            DateTimeOffset.UtcNow
+        )
     };
 }
