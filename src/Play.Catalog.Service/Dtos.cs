d6057b5bcf752e539b87abd1cb2212bd1b564aae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-03 21:16:27 +0300

    Added a range attribute to the Price parameter of UpdateItemDto

--- src/Play.Catalog.Service/Dtos.cs
+++ src/Play.Catalog.Service/Dtos.cs
@@ -5,4 +5,4 @@ public record ItemDto(Guid Id, string Name, string Description, decimal Price, D
 
 public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 
-public record UpdateItemDto([Required] string Name, string Description, decimal Price);
+public record UpdateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
