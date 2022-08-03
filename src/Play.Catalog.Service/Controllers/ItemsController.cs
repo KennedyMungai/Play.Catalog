6eec811d7b17ee9b59bcf88000f45453c6ff7f2d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-03 22:05:10 +0300

    Removed the list that was used for testing purposes

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -7,32 +7,6 @@ namespace Play.Catalog.Service.Controllers;
 [Route("items")]
 public class ItemsController : ControllerBase
 {
-    private static readonly List<ItemDto> items = new()
-    {
-        new ItemDto(
-            Guid.NewGuid(), 
-            "Potion", 
-            "Restores a small amount of HP", 
-            5, 
-            DateTimeOffset.UtcNow),
-
-        new ItemDto(
-            Guid.NewGuid(),
-            "Antidote",
-            "Cures poison",
-            7,
-            DateTimeOffset.UtcNow
-        ),
-
-        new ItemDto(
-            Guid.NewGuid(),
-            "Bronze Sword",
-            "Deals a small amount of damage",
-            5,
-            DateTimeOffset.UtcNow
-        )
-    };
-
     [HttpGet]
     public IEnumerable<ItemDto> Get()
     {
